b66b514226202914cb70f54d7ba96d6433bcec22
Sebastien <colladonsebastien@gmail.com>
2020-04-15 19:37:31 +0200

    Add tests for my part

--- __tests__/unit/attributes/cartesianLogarithmicAxis.test.js
+++ __tests__/unit/attributes/cartesianLogarithmicAxis.test.js
@@ -2,12 +2,9 @@ import CartesianLogarithmicAxis from 'c/cartesianLogarithmicAxis';
 import * as Handler from '../../__utils__/testAttributeHandler.js';
 
 const TEST_DATA_PROPERTIES = [
-  //new Handler.ChartOptionMock('text', 'foo', { display: true, text: 'foo' })
+  new Handler.ChartOptionMock('gridZ', 1, { yAxes: [{ gridLines: { z: 1 } }] })
 ];
 
-describe('c-cartesian-logarithmic-axis', () => {
-  //Handler.testAttribute(CartesianLogarithmicAxis, TEST_DATA_PROPERTIES);
+describe('c-layout', () => {
+  Handler.testAttribute(CartesianLogarithmicAxis, TEST_DATA_PROPERTIES);
 });
-
-// Remove after test is implemented
-test('', () => {});
--- __tests__/unit/attributes/cartesianTimeAxis.test.js
+++ __tests__/unit/attributes/cartesianTimeAxis.test.js
@@ -2,12 +2,42 @@ import CartesianTimeAxis from 'c/cartesianTimeAxis';
 import * as Handler from '../../__utils__/testAttributeHandler.js';
 
 const TEST_DATA_PROPERTIES = [
-  //new Handler.ChartOptionMock('text', 'foo', { display: true, text: 'foo' })
+  new Handler.ChartOptionMock('adapterDate', 'foo', {
+    yAxes: [{ adapters: { date: 'foo' } }]
+  }),
+  new Handler.ChartOptionMock('distribution', 'foo', {
+    yAxes: [{ distribution: 'foo' }]
+  }),
+  new Handler.ChartOptionMock('bounds', 'foo', { yAxes: [{ bounds: 'foo' }] }),
+  new Handler.ChartOptionMock('tickSource', 'foo', {
+    yAxes: [{ ticks: { source: 'foo' } }]
+  }),
+  new Handler.ChartOptionMock('timeDisplayformats', 'foo', {
+    yAxes: [{ time: { displayFormats: 'foo' } }]
+  }),
+  new Handler.ChartOptionMock('timeIsoweekday', true, {
+    yAxes: [{ time: { isoWeekday: true } }]
+  }),
+  new Handler.ChartOptionMock('timeParser', 'foo', {
+    yAxes: [{ time: { parser: 'foo' } }]
+  }),
+  new Handler.ChartOptionMock('timeRound', 'foo', {
+    yAxes: [{ time: { round: 'foo' } }]
+  }),
+  new Handler.ChartOptionMock('timeTooltipformat', 'foo', {
+    yAxes: [{ time: { tooltipFormat: 'foo' } }]
+  }),
+  new Handler.ChartOptionMock('timeUnit', '1', {
+    yAxes: [{ time: { unit: '1' } }]
+  }),
+  new Handler.ChartOptionMock('timeStepsize', 1, {
+    yAxes: [{ time: { stepSize: 1 } }]
+  }),
+  new Handler.ChartOptionMock('timeMinunit', '1', {
+    yAxes: [{ time: { minUnit: '1' } }]
+  })
 ];
 
-describe('c-cartesian-time-axis', () => {
-  //Handler.testAttribute(CartesianTimeAxis, TEST_DATA_PROPERTIES);
+describe('c-layout', () => {
+  Handler.testAttribute(CartesianTimeAxis, TEST_DATA_PROPERTIES);
 });
-
-// Remove after test is implemented
-test('', () => {});
