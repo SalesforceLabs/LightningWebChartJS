d72a4e309693c848d40dbed33c9d9d6cf7112305
Victor Garcia Zarco <victor.gzarco@gmail.com>
2020-04-21 19:50:37 +0200

    Clean rollup plugin for static resources

--- playground/rollup.config.js
+++ playground/rollup.config.js
@@ -3,29 +3,28 @@ const path = require('path');
 const lwc = require('@lwc/rollup-plugin');
 const replace = require('@rollup/plugin-replace');
 
-const STATIC_RESOURCE_PREFIX = '@salesforce/resourceUrl/';
+const STATIC_RESOURCE_ORIGIN = '@salesforce/resourceUrl/';
+const STATIC_RESOURCE_TARGET = `staticresources/`;
 
 function staticResource() {
   return {
     name: 'static-resource',
 
     // https://rollupjs.org/guide/en/#resolveid
-    resolveId(id) {
-      if (id.startsWith(STATIC_RESOURCE_PREFIX)) {
-        return `${id}.js`;
+    resolveId(source) {
+      if (source.startsWith(STATIC_RESOURCE_ORIGIN)) {
+        return `${source}.js`;
       }
     },
 
     // https://rollupjs.org/guide/en/#load
     load(id) {
-      if (!id.startsWith(STATIC_RESOURCE_PREFIX)) {
-        return;
+      if (id.startsWith(STATIC_RESOURCE_ORIGIN)) {
+        return `export default '${id.replace(
+          STATIC_RESOURCE_ORIGIN,
+          STATIC_RESOURCE_TARGET
+        )}'`;
       }
-
-      const resource = id.replace(STATIC_RESOURCE_PREFIX, '');
-      return `
-        export default '../src/staticresources/${resource}';
-      `;
     }
   };
 }
