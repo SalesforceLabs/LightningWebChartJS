fa9b2d52124d2b66666b3acca7b42a017ca26b33
Victor Garcia Zarco <victor.gzarco@gmail.com>
2020-04-22 11:50:11 +0200

    Add synthetic shadow doom to fix css injection problems

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,5 +1,5 @@
+import '@lwc/synthetic-shadow';
 import { createElement } from 'lwc';
-
 import Playground from 'app/playground';
 
 const playground = createElement('app-playground', { is: Playground });
