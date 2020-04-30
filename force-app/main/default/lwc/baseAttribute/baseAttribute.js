806d89c55eb134b72a022ec3ed350ab715eb5a51
Sebastien <colladonsebastien@gmail.com>
2020-05-01 00:34:57 +0200

    :pencil: Add some comments to base and principal components

--- force-app/main/default/lwc/baseAttribute/baseAttribute.js
+++ force-app/main/default/lwc/baseAttribute/baseAttribute.js
@@ -5,16 +5,19 @@ import ReactivityManager from 'c/reactivityManager';
 export default class BaseAttribute extends LightningElement {
   constructor() {
     super();
-    this._eventName = OPTION_EVENT_NAME;
-    this._reactivityManager = new ReactivityManager();
-    this._reactivityManager.registerJob(() => this.dispatchOption());
-    this._payload = this._reactivityManager.getReactivityProxy();
+    this._eventName = OPTION_EVENT_NAME; // option event
+    this._reactivityManager = new ReactivityManager(); // store reactivity manager in case need to throttle manually
+    this._reactivityManager.registerJob(() => this.dispatchOption()); // register dispatchOption
+    this._payload = this._reactivityManager.getReactivityProxy(); // use reactivity manager proxy
   }
 
+  // Attach parent to handle disconnectedCallback gracefully
   renderedCallback() {
     this._parent = this.template.host.parentNode;
   }
 
+  // disconnectedCallback is called after having remove template
+  // need to call the disconnect from on the parent
   disconnectedCallback() {
     if (this._parent) {
       this._parent.dispatchEvent(
@@ -29,6 +32,8 @@ export default class BaseAttribute extends LightningElement {
     }
   }
 
+  // dispatch the _eventName event to the parent
+  // Component catching the event will aggregate the detail
   dispatchOption() {
     this.dispatchEvent(
       new CustomEvent(this._eventName, {
--- force-app/main/default/lwc/reactivityManager/reactivityManager.js
+++ force-app/main/default/lwc/reactivityManager/reactivityManager.js
@@ -1,18 +1,28 @@
 export default class ReactivityManager {
   constructor() {
-    this._ready = false;
+    this._ready = false; // start as not ready
   }
 
   registerJob(cb) {
-    this.job = cb;
+    this.job = cb; // store one callback per reactivity manager
+    // Could be a good idea to store an array
+    // and to give the ability to Promise.all or to Promise.reduce
   }
 
+  // This method ensure to call once a job in the next micro task
+  // Used when it is possible and undeterminable to call multiple time
+  // a function which could be executed once at the end for every attempt
+  // without knowing in advance how much time it is going to be called
   throttleRegisteredJob() {
     if (!this._ready) {
-      this._ready = true;
+      // first call
+      this._ready = true; // then stop the possibility to ask for a callback
+      // wait for next micro task
       Promise.resolve().then(() => {
         if (this._ready) {
-          this._ready = false;
+          // check if ready (synchronism check)
+          this._ready = false; // come back to initial state
+          // Call the method to throttle
           if (typeof this.job === 'function') {
             this.job();
           }
@@ -21,6 +31,8 @@ export default class ReactivityManager {
     }
   }
 
+  // Create a proxy object which call the registered job
+  // when a property of the object is changed
   getReactivityProxy() {
     const reactivityProxy = {
       set: (obj, prop, value) => {
