95f059d903ab7f9efc2329a7350adf20834200c8
Sebastien <colladonsebastien@gmail.com>
2020-05-06 08:00:04 +0200

    Test reactivity manager

--- __tests__/unit/reactivityManager.test.js
+++ __tests__/unit/reactivityManager.test.js
@@ -1,16 +1,29 @@
-import { createElement } from 'lwc';
-
 import ReactivityManager from 'c/reactivityManager';
 
 describe('Reactivity Manager: ', () => {
-  test('register a job', () => {
-    expect(true).toBe(false);
-  });
-  test('throttle a job', () => {
-    expect(true).toBe(false);
-  });
+  const throttledFn = jest.fn();
+  const rm = new ReactivityManager();
+  rm.registerJob(throttledFn);
+  let reactivityProxy;
 
   test('get reactivity Proxy', () => {
-    expect(true).toBe(false);
+    reactivityProxy = rm.getReactivityProxy();
+    expect(reactivityProxy).toEqual({});
+    expect(throttledFn).not.toBeCalled();
+  });
+
+  test('proxy trigger throttling of the registered job', async () => {
+    reactivityProxy.test = 'test';
+    expect(throttledFn).not.toBeCalled();
+    await flushPromises().then(() => {
+      expect(throttledFn).toBeCalled();
+    });
+  });
+  test('manual call trigger throttling', async () => {
+    rm.throttleRegisteredJob();
+    expect(throttledFn).not.toBeCalled();
+    await flushPromises().then(() => {
+      expect(throttledFn).toBeCalled();
+    });
   });
 });
