784eca0ef2d57fabd23658b058f14f9a05e4e0d3
Victor Garcia Zarco <victor.gzarco@gmail.com>
2021-03-12 20:04:49 +0100

    Add more examples

--- force-app/main/default/lwc/structureSamples/structureSamples.js
+++ force-app/main/default/lwc/structureSamples/structureSamples.js
@@ -20,7 +20,7 @@ const DATASET_LABELS = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6'];
 //const DATASET_LABELS = ["Blue", "Orange", "Yellow", "Green", "Light Blue", "Purple"];
 
 //const GROUPS = ['First group', 'Second Group'];
-const GROUPS = ['Blue group', 'Red Group'];
+const GROUPS = ['Blue group', 'Red Group', 'Orange Group'];
 
 export default class StructureSamples extends LightningElement {
   barCharts = [
@@ -185,7 +185,7 @@ export default class StructureSamples extends LightningElement {
           COLOR_ORANGE,
           false
         ),
-        this.cDataItem(GROUPS[1], '[18, 10, 20, 10, 8, 4]', COLOR_YELLOW, false)
+        this.cDataItem(GROUPS[2], '[18, 10, 20, 10, 8, 4]', COLOR_YELLOW, false)
       ]
     },
     {
@@ -260,7 +260,7 @@ export default class StructureSamples extends LightningElement {
           COLOR_ORANGE,
           false
         ),
-        this.cDataItem(GROUPS[1], '[18, 10, 20, 10, 8, 4]', COLOR_YELLOW, false)
+        this.cDataItem(GROUPS[2], '[18, 10, 20, 10, 8, 4]', COLOR_YELLOW, false)
       ]
     },
     {
@@ -334,7 +334,457 @@ export default class StructureSamples extends LightningElement {
           COLOR_ORANGE,
           false
         ),
-        this.cDataItem(GROUPS[1], '[18, 10, 20, 10, 8, 4]', COLOR_YELLOW, false)
+        this.cDataItem(GROUPS[2], '[18, 10, 20, 10, 8, 4]', COLOR_YELLOW, false)
+      ]
+    },
+    {
+      title: 'Three datasets, items of same color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(GROUPS[0], '[12, 19, 7, 9, 5, 15]', [...ALL_COLORS]),
+        this.cDataItem(GROUPS[1], '[10, 15, 17, 12, 3, 4]', [...ALL_COLORS])
+      ]
+    },
+    {
+      title: 'Two datasets with missing items',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(GROUPS[0], '[12, 19, 7, 9, 5, 15, 8, 8]', [
+          COLOR_BLUE,
+          COLOR_ORANGE
+        ]),
+        this.cDataItem(GROUPS[1], '[10, 15, 17, 12]', [...ALL_COLORS])
+      ]
+    },
+    {
+      title: '*Without dataset labels - Makes sense ?',
+      datasetLabels: [''],
+      datasets: [
+        this.cDataItem(GROUPS[0], '[12, 19, 7, 9, 5, 15, 8, 8]', [
+          COLOR_BLUE,
+          COLOR_ORANGE
+        ]),
+        this.cDataItem(GROUPS[1], '[10, 15, 17, 12]', [
+          COLOR_ORANGE,
+          COLOR_PINK
+        ])
+      ]
+    },
+    {
+      title: 'From chart data provider',
+      ...this.parseDataProvider(
+        ['Item1', 'Item2', 'Item3'],
+        [3, 7, 6],
+        COLOR_BLUE
+      )
+    },
+    {
+      title: '*From chart data provider with no labels - No legend ?',
+      ...this.parseDataProvider([], [3, 7, 6], COLOR_BLUE)
+    }
+  ];
+
+  bubbleCharts = [
+    {
+      title: 'One dataset, items of same color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(
+          GROUPS[0],
+          [
+            { x: 3, y: 7, r: 10 },
+            { x: 5, y: 4, r: 5 },
+            { x: 3, y: 4, r: 5 }
+          ],
+          COLOR_BLUE
+        )
+      ]
+    },
+    {
+      title: 'One dataset, items of different color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(
+          GROUPS[0],
+          [
+            { x: 3, y: 7, r: 10 },
+            { x: 5, y: 4, r: 5 },
+            { x: 3, y: 4, r: 5 }
+          ],
+          [...ALL_COLORS]
+        ) // Force code readability
+      ]
+    },
+    {
+      title: 'Two datasets, groups of different color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(
+          GROUPS[0],
+          [
+            { x: 3, y: 7, r: 10 },
+            { x: 5, y: 4, r: 5 },
+            { x: 3, y: 4, r: 5 }
+          ],
+          COLOR_BLUE,
+          false
+        ),
+        this.cDataItem(
+          GROUPS[1],
+          [
+            { x: 6, y: 12, r: 11 },
+            { x: 4, y: 7, r: 1 },
+            { x: 2, y: 9, r: 3 }
+          ],
+          COLOR_ORANGE,
+          false
+        ),
+        this.cDataItem(
+          GROUPS[2],
+          [
+            { x: 8, y: 4, r: 3 },
+            { x: 1, y: 8, r: 3 },
+            { x: 2, y: 4, r: 9 }
+          ],
+          COLOR_YELLOW,
+          false
+        )
+      ]
+    },
+    {
+      title: 'Three datasets, items of same color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(
+          GROUPS[0],
+          [
+            { x: 3, y: 7, r: 10 },
+            { x: 5, y: 4, r: 5 },
+            { x: 3, y: 4, r: 5 }
+          ],
+          [...ALL_COLORS]
+        ),
+        this.cDataItem(
+          GROUPS[1],
+          [
+            { x: 6, y: 12, r: 11 },
+            { x: 4, y: 7, r: 1 },
+            { x: 2, y: 9, r: 3 }
+          ],
+          [...ALL_COLORS]
+        )
+      ]
+    },
+    {
+      title: 'Two datasets with missing items',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(
+          GROUPS[0],
+          [
+            { x: 3, y: 7, r: 10 },
+            { x: 5, y: 4, r: 5 }
+          ],
+          [COLOR_BLUE, COLOR_ORANGE]
+        ),
+        this.cDataItem(
+          GROUPS[1],
+          [
+            { x: 6, y: 12, r: 11 },
+            { x: 4, y: 7, r: 1 },
+            { x: 2, y: 9, r: 3 }
+          ],
+          [...ALL_COLORS]
+        )
+      ]
+    },
+    {
+      title: '*Without dataset labels - Makes sense ?',
+      datasetLabels: [''],
+      datasets: [
+        this.cDataItem(
+          GROUPS[0],
+          [
+            { x: 3, y: 7, r: 10 },
+            { x: 5, y: 4, r: 5 }
+          ],
+          [COLOR_BLUE, COLOR_ORANGE]
+        ),
+        this.cDataItem(
+          GROUPS[1],
+          [
+            { x: 6, y: 12, r: 11 },
+            { x: 4, y: 7, r: 1 },
+            { x: 2, y: 9, r: 3 }
+          ],
+          [COLOR_ORANGE, COLOR_PINK]
+        )
+      ]
+    },
+    {
+      title: 'From chart data provider',
+      ...this.parseDataProvider(
+        ['Item1', 'Item2', 'Item3'],
+        [3, 7, 6],
+        COLOR_BLUE
+      )
+    },
+    {
+      title: '*From chart data provider with no labels - No legend ?',
+      ...this.parseDataProvider([], [3, 7, 6], COLOR_BLUE)
+    }
+  ];
+
+  radarCharts = [
+    {
+      title: 'One dataset, items of same color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [this.cDataItem(GROUPS[0], '[12, 19, 7, 9, 5, 15]', COLOR_BLUE)]
+    },
+    {
+      title: 'One dataset, items of different color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(GROUPS[0], '[12, 19, 7, 9, 5, 15]', [...ALL_COLORS]) // Force code readability
+      ]
+    },
+    {
+      title: 'Two datasets, groups of different color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(GROUPS[0], '[12, 19, 7, 9, 5, 15]', COLOR_BLUE, false),
+        this.cDataItem(
+          GROUPS[1],
+          '[10, 15, 17, 12, 3, 4]',
+          COLOR_ORANGE,
+          false
+        ),
+        this.cDataItem(GROUPS[2], '[18, 10, 20, 10, 8, 4]', COLOR_YELLOW, false)
+      ]
+    },
+    {
+      title: 'Three datasets, items of same color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(GROUPS[0], '[12, 19, 7, 9, 5, 15]', [...ALL_COLORS]),
+        this.cDataItem(GROUPS[1], '[10, 15, 17, 12, 3, 4]', [...ALL_COLORS])
+      ]
+    },
+    {
+      title: 'Two datasets with missing items',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(GROUPS[0], '[12, 19, 7, 9, 5, 15, 8, 8]', [
+          COLOR_BLUE,
+          COLOR_ORANGE
+        ]),
+        this.cDataItem(GROUPS[1], '[10, 15, 17, 12]', [...ALL_COLORS])
+      ]
+    },
+    {
+      title: '*Without dataset labels - Makes sense ?',
+      datasetLabels: [''],
+      datasets: [
+        this.cDataItem(GROUPS[0], '[12, 19, 7, 9, 5, 15, 8, 8]', [
+          COLOR_BLUE,
+          COLOR_ORANGE
+        ]),
+        this.cDataItem(GROUPS[1], '[10, 15, 17, 12]', [
+          COLOR_ORANGE,
+          COLOR_PINK
+        ])
+      ]
+    },
+    {
+      title: 'From chart data provider',
+      ...this.parseDataProvider(
+        ['Item1', 'Item2', 'Item3'],
+        [3, 7, 6],
+        COLOR_BLUE
+      )
+    },
+    {
+      title: '*From chart data provider with no labels - No legend ?',
+      ...this.parseDataProvider([], [3, 7, 6], COLOR_BLUE)
+    }
+  ];
+
+  scatterCharts = [
+    {
+      title: 'One dataset, items of same color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(
+          GROUPS[0],
+          [
+            { x: 3, y: 7, r: 10 },
+            { x: 5, y: 4 },
+            { x: 3, y: 4 }
+          ],
+          COLOR_BLUE
+        )
+      ]
+    },
+    {
+      title: 'One dataset, items of different color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(
+          GROUPS[0],
+          [
+            { x: 3, y: 7, r: 10 },
+            { x: 5, y: 4 },
+            { x: 3, y: 4 }
+          ],
+          [...ALL_COLORS]
+        ) // Force code readability
+      ]
+    },
+    {
+      title: 'Two datasets, groups of different color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(
+          GROUPS[0],
+          [
+            { x: 3, y: 7, r: 10 },
+            { x: 5, y: 4 },
+            { x: 3, y: 4 }
+          ],
+          COLOR_BLUE,
+          false
+        ),
+        this.cDataItem(
+          GROUPS[1],
+          [
+            { x: 6, y: 12, r: 11 },
+            { x: 4, y: 7 },
+            { x: 2, y: 9 }
+          ],
+          COLOR_ORANGE,
+          false
+        ),
+        this.cDataItem(
+          GROUPS[2],
+          [
+            { x: 8, y: 4 },
+            { x: 1, y: 8 },
+            { x: 2, y: 4 }
+          ],
+          COLOR_YELLOW,
+          false
+        )
+      ]
+    },
+    {
+      title: 'Three datasets, items of same color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(
+          GROUPS[0],
+          [
+            { x: 3, y: 7, r: 10 },
+            { x: 5, y: 4 },
+            { x: 3, y: 4 }
+          ],
+          [...ALL_COLORS]
+        ),
+        this.cDataItem(
+          GROUPS[1],
+          [
+            { x: 6, y: 12, r: 11 },
+            { x: 4, y: 7 },
+            { x: 2, y: 9 }
+          ],
+          [...ALL_COLORS]
+        )
+      ]
+    },
+    {
+      title: 'Two datasets with missing items',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(
+          GROUPS[0],
+          [
+            { x: 3, y: 7, r: 10 },
+            { x: 5, y: 4 }
+          ],
+          [COLOR_BLUE, COLOR_ORANGE]
+        ),
+        this.cDataItem(
+          GROUPS[1],
+          [
+            { x: 6, y: 12, r: 11 },
+            { x: 4, y: 7 },
+            { x: 2, y: 9 }
+          ],
+          [...ALL_COLORS]
+        )
+      ]
+    },
+    {
+      title: '*Without dataset labels - Makes sense ?',
+      datasetLabels: [''],
+      datasets: [
+        this.cDataItem(
+          GROUPS[0],
+          [
+            { x: 3, y: 7, r: 10 },
+            { x: 5, y: 4 }
+          ],
+          [COLOR_BLUE, COLOR_ORANGE]
+        ),
+        this.cDataItem(
+          GROUPS[1],
+          [
+            { x: 6, y: 12, r: 11 },
+            { x: 4, y: 7 },
+            { x: 2, y: 9 }
+          ],
+          [COLOR_ORANGE, COLOR_PINK]
+        )
+      ]
+    },
+    {
+      title: 'From chart data provider',
+      ...this.parseDataProvider(
+        ['Item1', 'Item2', 'Item3'],
+        [3, 7, 6],
+        COLOR_BLUE
+      )
+    },
+    {
+      title: '*From chart data provider with no labels - No legend ?',
+      ...this.parseDataProvider([], [3, 7, 6], COLOR_BLUE)
+    }
+  ];
+
+  polarAreaCharts = [
+    {
+      title: 'One dataset, items of same color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [this.cDataItem(GROUPS[0], '[12, 19, 7, 9, 5, 15]', COLOR_BLUE)]
+    },
+    {
+      title: 'One dataset, items of different color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(GROUPS[0], '[12, 19, 7, 9, 5, 15]', [...ALL_COLORS]) // Force code readability
+      ]
+    },
+    {
+      title: 'Two datasets, groups of different color',
+      datasetLabels: DATASET_LABELS,
+      datasets: [
+        this.cDataItem(GROUPS[0], '[12, 19, 7, 9, 5, 15]', COLOR_BLUE, false),
+        this.cDataItem(
+          GROUPS[1],
+          '[10, 15, 17, 12, 3, 4]',
+          COLOR_ORANGE,
+          false
+        ),
+        this.cDataItem(GROUPS[2], '[18, 10, 20, 10, 8, 4]', COLOR_YELLOW, false)
       ]
     },
     {
@@ -398,5 +848,13 @@ export default class StructureSamples extends LightningElement {
       datasetLabels: labels,
       datasets: [this.cDataItem('UniqueGroup', detail, bgColor)]
     };
+  }
+
+  get circumference() {
+    return 1 * Math.PI;
+  }
+
+  get rotation() {
+    return -1 * Math.PI;
   }
 }
